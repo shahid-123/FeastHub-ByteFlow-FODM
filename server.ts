0b759efce6ed55303c23896df361269a8f380fb9
shahid-123 <44270184+shahid-123@users.noreply.github.com>
2026-05-20 19:26:49 +0530

    Update server.ts

--- server.ts
+++ server.ts
@@ -357,7 +357,8 @@ async function startServer() {
   const app = express();
   app.use(express.json());
 
-  const PORT = 3000;
+  //const PORT = 3000;
+  const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3000;
 
   // --- REST API ENDPOINTS ---
 
